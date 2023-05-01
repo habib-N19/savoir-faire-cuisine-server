64a6f24f1822e44924de39e032e78387e2230b83
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-02 03:50:35 +0600

    added local server api for home page data and single chef data

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+const chefInfos = require('./data/chefsData.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
-
+app.get('/chefInfos', (req, res) => {
+    res.send(chefInfos)
+})
+// single chef info 
+app.get('/chefInfos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedChef = news.find(n => n.id === id);
+    res.send(selectedChef);
+})
 app.listen(port, () => {
     console.log(`Chef API is running on port : ${port}`);
 })
