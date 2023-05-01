6337107a6a749ad440db83c950129202d3742b47
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-02 03:36:54 +0600

    added basic functionality of a express server , no data imported or loaded yet!

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`Chef API is running on port : ${port}`);
+})
